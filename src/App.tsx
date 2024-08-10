d1bd1172c6686d60459bacfe67abace5ea145b66
Dipak-Gautam <anjangautam095@gmail.com>
2024-08-10 14:41:30 +0545

    got idea

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
+import bgimage from "./bgImage/herobg.jpg";
+
 function App() {
   return (
-    <>
-      <div className="font-heebo">hello</div>
-    </>
+    <div>
+      <div
+        className=" h-[70vh] bg-cover bg-center"
+        style={{ backgroundImage: `url(${bgimage})` }}
+      >
+        <div className="h-full flex justify-between md:mx-24">
+          <div className="  flex flex-col justify-center space-y-2   ">
+            <p className=" text-lg text-slate-300 font-heebo font-semibold">
+              Hi, I'm
+            </p>
+            <p className="text-5xl font-bold font-heebo text-white">
+              Dipak Gautam
+            </p>
+            <p className=" text-base text-slate-300 font-heebo font-semibold">
+              A passionate Front-End React Developer
+            </p>
+          </div>
+          <div className="h-full">
+            <p className=" h-full rounded-full overflow-auto -translate-y-20 ">
+              <img
+                src="public/assets/images/nobgprofile.png"
+                alt=""
+                className="h-[95%] translate-y-4 "
+              />
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
